3a39aa6d703de728c28bd419bf1d0dac01df3c07
Avam <avamkansal@gmail.com>
2026-06-11 01:02:44 +0530

    Day 2- authentication system

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
+const connectDB = require("./config/db");
 
 dotenv.config();
+
+connectDB();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Document Signature API Running");
+  res.send("API Running");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
